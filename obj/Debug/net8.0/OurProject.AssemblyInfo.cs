6d1b939ac294134c73898003c790906ab6bc9864
Pratheek <pratheek212006@gmail.com>
2026-05-05 21:30:40 +0530

    Removed bin and obj properly

--- obj/Debug/net8.0/OurProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/OurProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OurProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa5ab14931cf6f92df566f284ef0041dfd666f2f")]
 [assembly: System.Reflection.AssemblyProductAttribute("OurProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OurProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
